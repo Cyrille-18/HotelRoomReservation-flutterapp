d396a3ab2963fcb4a0f80fc8950886408fc5c6dd
Cyrille-18 <cyrillepio@hotmail.com>
2024-06-17 18:24:29 +0000

    roomcard

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -11,6 +11,7 @@ import 'package:hotelroomreservation/constantes.dart';
 import 'package:hotelroomreservation/models/room.dart';
 import 'package:hotelroomreservation/screens/room_details_screen.dart';
 
+import '../utils/roomcard.dart';
 import '../view/navigationbar.dart';
 import '../widgets/room_card.dart';
 
@@ -86,9 +87,6 @@ class _HomepageState extends State<Homepage> {
                   Stack(
                     children: <Widget>[RoomCard()],
                   ),
-                  Stack(
-                    children: <Widget>[RoomCard()],
-                  )
                 ],
               ),
             ),
@@ -99,105 +97,6 @@ class _HomepageState extends State<Homepage> {
         ),
       ),
       bottomNavigationBar: navigationbar(),
-    );
-  }
-}
-
-class RoomCard extends StatelessWidget {
-  const RoomCard({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    return Container(
-      margin: EdgeInsets.symmetric(
-        horizontal: kDefaultPadding,
-        vertical: kDefaultPadding / 2,
-      ),
-      // color: Colors.blueAccent,
-      height: 160,
-      child: Stack(
-        alignment: Alignment.bottomCenter,
-        children: <Widget>[
-          //arriere-plan
-          Container(
-            height: 135,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(22),
-              color: kBlueColor,
-              boxShadow: [kDefaultShadow],
-            ),
-            child: Container(
-              margin: EdgeInsets.only(right: 10),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(22),
-              ),
-            ),
-          ),
-          // image de la chambre
-          Positioned(
-              top: 25,
-              right: 0,
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(22),
-                child: Container(
-                  padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
-                  height: 135,
-                  width: 230,
-                  child: Image.asset(
-                    "assets/images/chbre6.jpg",
-                    fit: BoxFit.cover,
-                  ),
-                ),
-              )),
-          //Nom et Description de la chambre
-          Positioned(
-            bottom: 0,
-            left: 0,
-            child: SizedBox(
-              height: 136,
-              width: size.width - 200,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Spacer(),
-                  Padding(
-                    padding:
-                        const EdgeInsets.symmetric(horizontal: kDefaultPadding),
-                    child: Text(
-                      "Chambre luxieuse Parisienne",
-                      style: Theme.of(context).textTheme.labelLarge,
-                    ),
-                  ),
-                  Spacer(),
-                  Container(
-                    padding: EdgeInsets.symmetric(
-                      horizontal: kDefaultPadding * 1.5, //30 padding
-                      vertical: kDefaultPadding / 4, // 5 en haut 5 en bas
-                    ),
-                    decoration: BoxDecoration(
-                      color: Colors.black,
-                      borderRadius: BorderRadius.only(
-                          bottomLeft: Radius.circular(22),
-                          topRight: Radius.circular(22)),
-                    ),
-                    child: Text(
-                      "58000 \FCFA",
-                      style: Theme.of(context)
-                          .textTheme
-                          .labelLarge
-                          ?.copyWith(color: Colors.white),
-                    ),
-                  )
-                ],
-              ),
-            ),
-          ),
-        ],
-      ),
     );
   }
 }
